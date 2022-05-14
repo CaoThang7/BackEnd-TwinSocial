2dff1a421c9d0063b6865b95f3ad68c417c3919a
CaoThang <thangly2k1@gmail.com>
2022-05-14 20:23:22 +0700

    set up create post

---
+++ controllers/postCtrl.js
@@ -0,0 +1,28 @@
+const Posts = require('../models/postModel')
+const postCtrl = {
+    createPost: async (req, res) => {
+        try {
+            const { userId, content, images } = req.body
+
+            if (images.length === 0)
+                return res.status(400).json({ msg: "Please add your photo." })
+
+            const newPost = new Posts({
+                userId, content, images
+            })
+            await newPost.save()
+
+            res.json({
+                msg: 'Created Post!',
+                newPost: {
+                    ...newPost._doc,
+                    user: req.user
+                }
+            })
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    },
+}
+
+module.exports = postCtrl
